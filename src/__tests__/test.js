7815e63cc3ddcf93cd3d79948793652ef1b980a8
Pavel Chekalin <grokki91@gmail.com>
2023-01-24 21:39:50 +0300

    Add tests

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -5,11 +5,28 @@ import Character from '../app/Character';
 describe(('Check iterable function'), () => {
   const bowman = new Character('Лучник', 'Bowman');
   const demon = new Character('Демон', 'Demon');
-  const mag = new Character('Маг', 'Mag');
-  const warrior = new Character('Воин', 'Warrior');
   const team = new Team();
-  team.addAll(bowman, demon, mag, warrior);
+  team.addAll(bowman, demon);
   test(('Check correct result'), () => {
-    
+    const arr = [];
+    const expectedResult = [{
+      name: 'Лучник',
+      type: 'Bowman',
+      health: 100,
+      level: 1,
+      attack: 100,
+      defence: 100,
+    }, {
+      name: 'Демон',
+      type: 'Demon',
+      health: 100,
+      level: 1,
+      attack: 100,
+      defence: 100,
+    }];
+    for (const hero of iterable(team)) {
+      arr.push(hero);
+    }
+    expect(arr).toEqual(expectedResult);
   });
 });
--- src/app/Team.js
+++ src/app/Team.js
@@ -3,18 +3,7 @@ export default class Team {
     this.members = new Set();
   }
 
-  add(character) {
-    if (character in this.members) {
-      new Error('Такой персонаж уже есть');
-    }
-    this.members.add(character);
-  }
-
   addAll(...characters) {
     characters.forEach((character) => this.members.add(character));
-  }
-
-  toArray() {
-    return [...this.members];
   }
 }
