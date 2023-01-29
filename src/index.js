c8ba7bc739a57770972ae280aa25e288a95ae01d
Pavel Chekalin <grokki91@gmail.com>
2023-01-29 18:56:52 +0300

    Delete tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Team from './app/Team';
 import Character from './app/Character';
+import iterable from './app/iterable';
 
 const bowman = new Character('Лучник', 'Bowman');
 const demon = new Character('Демон', 'Demon');
@@ -7,3 +8,7 @@ const mag = new Character('Маг', 'Mag');
 const warrior = new Character('Воин', 'Warrior');
 const team = new Team();
 team.addAll(bowman, demon, mag, warrior);
+
+for (const hero of iterable(team)) {
+  console.log(hero);
+}
